f1ee2dee3b5a7c585be7245337b83c4193139249
simonemarketti <simomarketti@icloud.com>
2022-02-18 12:17:28 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Password Tree
+
+## Overview
+
+A password validator that changes the growth state of the tree depending on the requirements reached.
+
+### Concepts
+
+- Form creation
+- Regex validation
+- Conditional rendering
+- DOM traversing
+
+### Languanges and tools
+
+- HTML
+- CSS
+- JAVASCRIPT
+- JQUERY
+- GIT
+- NPM
+- BABEL
+- AUTOPREFIXER
+- REGEX 101.COM
+
+### Links
+
+https://simonemarketti.github.io/password-tree
+
+### Notes
+
+Based on “Password strength verification” project (https://www.webdesignerdepot.com/2012/01/password-strength-verification-with-jquery/)
---
+++ src/script.js
@@ -0,0 +1,53 @@
+$(function () {
+  // initial animation
+  $("#img-container").children().eq(0).fadeIn(3000);
+
+  $("#pswd").keyup(function () {
+    // set password strength
+    let strength = 0;
+    // set password variable
+    let pswd = $(this).val();
+
+    //validate length
+    if (pswd.length > 8) {
+      $("#length").removeClass("invalid").addClass("valid");
+      strength += 1;
+    } else {
+      $("#length").removeClass("valid").addClass("invalid");
+    }
+
+    //validate letter
+    if (pswd.match(/[A-z]/)) {
+      $("#letter").removeClass("invalid").addClass("valid");
+      strength += 1;
+    } else {
+      $("#letter").removeClass("valid").addClass("invalid");
+    }
+
+    //validate capital letter
+    if (pswd.match(/[A-Z]/)) {
+      $("#capital").removeClass("invalid").addClass("valid");
+      strength += 1;
+    } else {
+      $("#capital").removeClass("valid").addClass("invalid");
+    }
+
+    //validate number
+    if (pswd.match(/\d/)) {
+      $("#number").removeClass("invalid").addClass("valid");
+      strength += 1;
+    } else {
+      $("#number").removeClass("valid").addClass("invalid");
+    }
+
+    // conditional rendering image
+    $("#img-container").children().hide().eq(strength).show();
+
+    // conditional formatting button
+    if (strength != 4) {
+      $("#submit").prop("disabled", true);
+    } else {
+      $("#submit").prop("disabled", false);
+    }
+  });
+});
